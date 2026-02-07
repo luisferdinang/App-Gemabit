4761004fb210fdaa6bcf738a4f9b5ca906e34635
Luis Ferdinand <salga@Luisferdinand.com>
2026-02-06 20:41:41 -0400

    Fix: bump SW version to v2 to force Vercel update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gemabit-v1';
+const CACHE_NAME = 'gemabit-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -8,9 +8,21 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
     })
   );
 });
